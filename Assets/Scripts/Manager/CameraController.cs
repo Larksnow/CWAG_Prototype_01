5ad1b08c02d9cf0af43f72637e3b06ddba27476f
Alan Fan <fwt2584190223@gmail.com>
2025-01-14 22:28:44 -0500

    Build Template scene

---
+++ Assets/Scripts/Manager/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Singleton object
+
+    public static CameraController main;
+    public CinemachineVirtualCamera virtualCamera;
+    
+    private void Awake()
+    {
+        if (main) Destroy(gameObject);
+        else main = this;
+    }
+
+    public void Shake(float duration, float magnitude)
+    {
+        StartCoroutine(ShakeCoroutine(duration, magnitude));    
+    }
+    
+    IEnumerator ShakeCoroutine(float duration, float magnitude)
+    {
+        // move the camera forwards and backward
+        CinemachineBasicMultiChannelPerlin perlin = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        perlin.m_AmplitudeGain = magnitude;
+        yield return new WaitForSeconds(duration);
+        perlin.m_AmplitudeGain = 0;
+    }
+}
---
+++ Assets/Scripts/Manager/MouseManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MouseManager : MonoBehaviour
+{
+    public RectTransform crosshair;
+    // Update is called once per frame
+    void Update()
+    {
+        // Print out the position of the mouse
+        // When the mouse moves, print out the position of the mouse
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            UpdatePointerPosition();
+        }
+
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.F4))
+        {
+            Application.Quit();
+        }
+
+    }
+
+    public void UpdatePointerPosition()
+    {
+            
+        // Convert the mouse position to a canvas position
+        float scaling =  1920f / Screen.width;
+        Vector2 canvasPosition = Input.mousePosition * scaling;
+            
+        canvasPosition.x = canvasPosition.x - (Screen.width / 2) * scaling;
+        canvasPosition.y = canvasPosition.y - (Screen.height / 2) * scaling;
+        
+        UpdateCrosshairPostiton(canvasPosition);
+    }
+
+    public void UpdateCrosshairPostiton(Vector2 screenPosition)
+    {
+        crosshair.anchoredPosition = screenPosition;
+    }
+
+    public Vector2 GetCrosshairPosition()
+    {
+        return crosshair.anchoredPosition;
+    }
+}
