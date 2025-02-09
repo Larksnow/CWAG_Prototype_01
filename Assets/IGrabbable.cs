0fa0af5084936871884f349b35534d5a608c6f09
edwardhuahan <edwardhuahan@gmail.com>
2025-02-08 21:38:50 -0500

    Add tweening

--- Assets/IGrabbable.cs
+++ Assets/IGrabbable.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class IGrabbable : MonoBehaviour
 {
 
     private Transform prevParent;
+    private Rigidbody rb;
+
+    public float pushForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     /// <summary>
     /// This function is called when the object is grabbed. Should move the object to the player's hand.
@@ -14,14 +23,26 @@ public class IGrabbable : MonoBehaviour
     /// <param name="position">The position of the player's hand</param>
     public void OnGrab(Transform grabHand)
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
         // Move the object to the player's hand
-        transform.position = grabHand.position;
-        // Set it as a child of the player's hand
+        // transform.position = grabHand.position;
+        // Trigger a DOTween to move the object to the player's hand
+        Vector3 direction = grabHand.position - transform.position;
+        rb.AddForce(-direction.normalized * pushForce, ForceMode.Impulse);
+
+        // Call MoveTo in 1 second using Coroutine
+        StartCoroutine(MoveToCoroutine(grabHand));
+    }
+
+    private IEnumerator MoveToCoroutine(Transform grabHand)
+    {
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Moving to hand");
+        rb.velocity = Vector3.zero;
+        transform.DOMove(grabHand.position, 0.2f).SetEase(Ease.OutSine);
         prevParent = transform.parent;
         transform.SetParent(grabHand);
-
-        // Enable kinetic
-        Rigidbody rb = GetComponent<Rigidbody>();
         rb.isKinematic = true;
     }
 
